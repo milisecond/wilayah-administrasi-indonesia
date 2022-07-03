6a150a3dbb86bd3187f2e51f534999bea0fef1c4
zidnim5 <zidni.mujib5@gmail.com>
2022-07-03 21:42:10 +0700

    Add migration table

--- api/database/migrations/2022_07_03_143536_create_table_region.php
+++ api/database/migrations/2022_07_03_143536_create_table_region.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('tbl_region', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('kode', 25)->unique();
+            $table->text('nama');
+            $table->timestamps();
         });
     }
 
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('table_region');
     }
 };
